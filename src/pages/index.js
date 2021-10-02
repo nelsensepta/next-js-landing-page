03ec47af9c517c13b7deae4fd7a801207a09f402
nelsesepta <nelseseptahenidar@gmail.com>
2021-10-03 02:27:29 +0700

    Add Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Extend from "../components/Extend";
 import FAQ from "../components/FAQ";
+import Head from "next/head";
 import Contact from "../components/Contact";
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Travel One Page</title>
+      </Head>
       <Hero />
       <Features />
       <Extend />
